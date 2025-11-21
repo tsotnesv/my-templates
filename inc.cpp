47f5b41b2f0bd8a7e1b886036af7e85b83d349c5
tsotnesv <112477768+tsotnesv@users.noreply.github.com>
2025-11-21 15:15:24 +0400

    Add initial implementation of inc.cpp

---
+++ inc.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣿⠛⣿⠀⠀⠀⠀⣤⣿⢻⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣿⡛⠀⣤⣿⣿⣤⣤⣿⣿⣤⢸⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡗⠀
+⢠⣼⣿⣿⣿⣿⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷
+⢸⣿⣿⡟⠛⠛⢿⣿⣿⣿⣿⣿⣿⣿⣤⣤⣤⣿⣿⣿⣿⣤⣤⣼⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠋       
+*/
+#define fi first
+#define se second
+#define pb push_back
+#define ins insert
+#define sz(a) (int)(a.size())
+#define all(x) (x).begin(),(x).end()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+void print(){cout<<'\n';}
+template<class h,class...t>
+void print(const h&v,const t&...u){cout<<v<<' ',print(u...);}
+
+//#define ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...)
+#endif
+
+//const int mod = 1e9+7;
+//const int mod = 998244353;
+const int MAXN=2e5+5; 
+const ll inf=1e9,INF=1e18; 
+
+
+
+void solve(int tc = 0){
+    
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    int tc=1;
+    //cin>>tc;
+    for(int t = 0; t < tc; t++) solve(t);
+}
